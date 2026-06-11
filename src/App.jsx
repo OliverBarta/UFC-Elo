c8f9b5d3a96064ddcc189189538b4e77d4334be6
OliverBarta <barta3738@gmail.com>
2026-06-11 14:49:19 -0400

    added searchbar and filter button (they dont do anything)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import TopBar from './TopBar'
 
+
 import './App.css'
 
 function App() {
@@ -9,7 +10,13 @@ function App() {
 
   return (
     <>
-      <p></p>
+      <h1>UFC Elo</h1>
+      <div className='searchArea'>
+        <button className='filtersToggle'>Filters</button>
+        <input type="text" placeholder="Enter fighter name" autoComplete="off" className='searchBar' />
+      </div>
+
+
     </>
   )
 }
