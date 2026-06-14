e8ea7088e346f856e8eac1655bbc36de029a1460
OliverBarta <barta3738@gmail.com>
2026-06-13 23:52:58 -0400

    reorganizing in preperation for filter menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import TopBar from './TopBar'
 import FighterList from './FighterList'
@@ -7,17 +7,32 @@ import FighterList from './FighterList'
 import './App.css'
 
 function App() {
+  const [filterIsOpen, setFilterIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 900);
   
+  // toggles the filter open and closed
+  const toggleFilterMenu = () => {
+    setFilterIsOpen(!filterIsOpen);
+  };
+
+      // Checks for resized windows
+      useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth < 900);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+
+    }, []);
 
   return (
     <>
-      <h1>UFC Elo</h1>
-      <div className='searchArea'>
-        <button className='filtersToggle'>Filters</button>
-        <input type="text" placeholder="Enter fighter name" autoComplete="off" className='searchBar' />
+      <div className='mainArea'>
+        <h1>UFC Elo</h1>
+        <div className='searchArea'>
+          <button className='filtersToggle'>Filters</button>
+          <input type="text" placeholder="Enter fighter name" autoComplete="off" className='searchBar' />
+        </div>
+        <FighterList/>
       </div>
-      <FighterList/>
-
 
     </>
   )
