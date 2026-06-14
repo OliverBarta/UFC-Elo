9db7ca8d5c6e527f23a31fbbb2679d70c05de852
OliverBarta <barta3738@gmail.com>
2026-06-14 01:05:47 -0400

    filter menu and very fancy hover animations

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,32 @@ function App() {
     setFilterIsOpen(!filterIsOpen);
   };
 
-      // Checks for resized windows
-      useEffect(() => {
-        const handleResize = () => setIsMobile(window.innerWidth < 900);
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
+  // Checks for resized windows
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 900);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
 
-    }, []);
+  }, []);
+
+  const shouldShowMobileFilter = filterIsOpen && isMobile;
 
   return (
     <>
+      {(!isMobile || filterIsOpen) && <div className='filterArea'>
+        <div>test</div>
+        {isMobile && <button className='filterTogglePop' onClick={toggleFilterMenu}>Close</button>}
+      </div>}
+      {!isMobile && <div className='filterAreaInvisible'>
+      </div>}
+      
       <div className='mainArea'>
         <h1>UFC Elo</h1>
         <div className='searchArea'>
-          <button className='filtersToggle'>Filters</button>
-          <input type="text" placeholder="Enter fighter name" autoComplete="off" className='searchBar' />
+          {isMobile && <button className='searchBarContainer' style={{width: "85px"}} onClick={toggleFilterMenu}>Filters</button>}
+          <div className='searchBarContainer'>
+            <input type="text" placeholder="Enter fighter name" autoComplete="off" className='searchBar' />
+          </div>
         </div>
         <FighterList/>
       </div>
